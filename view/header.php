98db267524b5e19971829a8baa5295607c1397b4
Luis Cedano <king.romanus1@gmail.com>
2014-11-12 10:44:37 -0800

    few

--- view/header.php
+++ view/header.php
@@ -1,3 +1,5 @@
+<!DOCTYPE html>
+<meta name="viewport" content="minimal-ui, width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"> 
 <html>
     <head>
         <meta charset="UTF-8">
